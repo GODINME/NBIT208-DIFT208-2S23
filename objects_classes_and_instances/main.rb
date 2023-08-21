4c108f57f7ae95bc3915160366bcd2137e00605c
= <=>
2023-08-21 15:30:43 +0000

    updated main and employee file for the 7Th time

--- objects_classes_and_instances/main.rb
+++ objects_classes_and_instances/main.rb
@@ -4,7 +4,7 @@ paul = Employee.new(1, "Paul", 34)
 patrick = Employee.new(2, "Patrick", 26)
 eric = Employee.new(3, "Eric", 45)
 
-
-require 'pry'; binding.pry
-
-paul.id
+# require 'pry'; binding.pry
+puts paul.salary
+paul.increase_salary(300)
+puts paul.salary
