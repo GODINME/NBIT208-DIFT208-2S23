eb9a54afb8658e5d9188977bbb6bd0d5f9a9f181
= <=>
2023-08-21 13:38:35 +0000

    updated both my main and student file for the 5th time

--- objects_classes_and_instances/employee.rb
+++ objects_classes_and_instances/employee.rb
@@ -1,8 +1,24 @@
 class Employee 
-    def def initialize(id, name, age)
+    def initialize(id, name, age)
         @id = id 
         @name = name 
         @age = age
         @retirement_age = 70 
+    end
+
+    def id 
+        @id
+    end
+
+    def name 
+        @name 
+    end
+
+    def age
+        @age 
+    end
+
+    def retire_age 
+        @retirement_age
     end
 end
--- objects_classes_and_instances/main.rb
+++ objects_classes_and_instances/main.rb
@@ -4,6 +4,7 @@ paul = Employee.new(1, "Paul", 34)
 patrick = Employee.new(2, "Patrick", 26)
 eric = Employee.new(3, "Eric", 45)
 
+
 require 'pry'; binding.pry
 
 paul.id
